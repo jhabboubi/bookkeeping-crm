982e1b4872f8ec5130ac179dcf127f2ed296678e
Jafer M. AlHaboubi <habboubi@gmail.com>
2020-03-26 15:34:05 -0500

    added security control of views via thymelead sec

--- crm/src/main/java/com/fluidcodes/crm/CrmApplication.java
+++ crm/src/main/java/com/fluidcodes/crm/CrmApplication.java
@@ -2,7 +2,11 @@ package com.fluidcodes.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import com.fluidcodes.crm.dao.UsersRepo;
 
@@ -10,6 +14,18 @@ import com.fluidcodes.crm.dao.UsersRepo;
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = UsersRepo.class)
 public class CrmApplication {
+	
+	@Bean
+	public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver, SpringSecurityDialect sec) {
+	    final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+	    templateEngine.setTemplateResolver(templateResolver);
+	    templateEngine.addDialect(sec); // Enable use of "sec"
+	    return templateEngine;
+	}
+	@Bean
+    public SpringSecurityDialect securityDialect() {
+        return new SpringSecurityDialect();
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrmApplication.class, args);
